c62539e32f5ca4a61c5a32e3272a96b9636f2c8a
G0V1NDS <connect.govinds@gmail.com>
2024-02-21 19:11:11 +0530

    Restoring previous session by default

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -12,6 +12,9 @@ in {
     firefox = {
       enable = true;
       profiles."${vars.user}" = {
+        id = 0;
+        name = "${vars.user}";
+        isDefault = true;
         # extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
           vimium
@@ -37,16 +40,21 @@ in {
           "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
           "browser.newtabpage.activity-stream.showSponsored" = false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.newtabpage.enabled" = false; # Make new tabs blank
           "browser.newtabpage.pinned" = false;
           "browser.protections_panel.infoMessage.seen" = true;
           "browser.quitShortcut.disabled" = true;
           "browser.shell.checkDefaultBrowser" = false;
           "browser.ssb.enabled" = true;
+          "browser.startup.page" = 3; # Restore previous session
           "browser.toolbars.bookmarks.visibility" = "never";
           "datareporting.policy.dataSubmissionEnable" = false;
           "datareporting.policy.dataSubmissionPolicyAcceptedVersion" = 2;
+          "dom.forms.autocomplete.formautofill" = false; # Disable autofill
+          "dom.payments.defaults.saveAddress" = false; # Disable address save
           "dom.security.https_only_mode" = true;
           "dom.security.https_only_mode_ever_enabled" = true;
+          "extensions.formautofill.creditCards.enabled" = false; # Disable credit cards
           "identity.fxaccounts.enabled" = false;
           "privacy.trackingprotection.enabled" = true;
           "privacy.trackingprotection.socialtracking.enabled" = true;
